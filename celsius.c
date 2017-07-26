2b8c8b66c50f2022348727144b8f7d6cb71ba11d
DharanyaN <30457114+DharanyaN@users.noreply.github.com>
2017-07-26 18:00:38 +0530

    Update celsius.c

--- celsius.c
+++ celsius.c
@@ -1,12 +1,9 @@
 #include<stdio.h>
-#include<conio.h>
-#include<math.h>
 int main()
 {
 float a,b;
-clsscr();
 printf("enter the celsius:");
 scanf("%f",&a);
-b=(a*1.8)+32
+b=(a*1.8)+32;
 printf("%f",b);
 }
